5947c853d73e486062237b9d649a8c4777723ec8
S1lverH4ze1337 <116435800+S1lverH4ze1337@users.noreply.github.com>
2024-04-18 20:30:26 +0200

    added uvicorn for the start

--- packages/main/main.py
+++ packages/main/main.py
@@ -1,4 +1,5 @@
 import sys
+import uvicorn
 from fastapi import FastAPI, Request
 from typing import Dict, Any
 sys.path.append('C:/Users/lenna/LokaleDaten/applied_project/tradingbot/packages')
@@ -129,3 +130,6 @@ async def get_rsi(request: Request)-> Dict[str, Any]:
     }
 
     return result
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port='8000', reload=True)
